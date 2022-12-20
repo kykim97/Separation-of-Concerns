8fbb46f5407ca1e2cfc2d3ae3cd21825e29357ca
kykim97 <113568664+kykim97@users.noreply.github.com>
2022-12-20 15:26:53 +0900

    Update Application.java

--- separation/of.concerns/main/Application.java
+++ separation/of.concerns/main/Application.java
@@ -2,6 +2,13 @@ package separation.of.concerns.main;
 
 public class Application {
     public static void main(Staring[] args){
-        
+        Presentation presentation = new Presentation(
+            new BusinessLogic(
+                new Database()
+            )
+        );
+
+        presentation.doPresentation();
+
     }
 }
