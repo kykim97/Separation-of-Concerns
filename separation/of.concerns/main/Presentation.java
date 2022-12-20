a72ed08e29256c9acf47c09e2c5247c745e50232
kykim97 <113568664+kykim97@users.noreply.github.com>
2022-12-20 15:25:08 +0900

    Create Presentation.java

---
+++ separation/of.concerns/main/Presentation.java
@@ -0,0 +1,13 @@
+package separation.of.concerns.main;
+
+public class Presentation {
+
+    public Presentation(BusinessLogic BusinessLogic) {
+
+    }
+
+    public void doPresentation() {
+
+    }
+
+}
